d6953008e84101e63bdc7c7a759cec1e08a9426e
Andrew Semikov <gep@gep-work.(none)>
2010-05-09 17:58:42 +0300

    Filter initial

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/var/www/gepfilter/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
